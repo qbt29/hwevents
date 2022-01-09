bdeb8f1a44fb355c3e0263a7dad6b7e03f63fcf9
qbt29 <zubakov.yaroslaw@yandex.ru>
2022-01-09 22:13:59 +0300

    Delete git backups

--- main.py
+++ main.py
@@ -86,6 +86,9 @@ def request(msg):
         with open("servers.py", "w") as f:
             f.write(str(servers))
             f.write('\n' + str(targets))
+        os.system('git add *.py')
+        os.system('git commit -m "Back up"')
+        os.system("git push https://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@github.com/qbt29/hwevents.git")
     unique=0
     for message in msg['fwd_messages']:
         text,from_id,date=message['text'],message['from_id'],message['date']
